2d245e00a9fcd39f373ee0d2e44c27ba8d28969a
Brandon-Neves <84950033+Brandon-Neves@users.noreply.github.com>
2023-05-21 15:16:22 -0300

    feat: create routes signin

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -5,9 +5,18 @@ import bcrypt from 'bcrypt'
 export async function LoginUser(req, res) {
   const body = req.body
   const { id, password } = res.locals.login
-  console.log(body.password)
 
   try {
+    if (body.password !== password) {
+      return res.sendStatus(401)
+    }
+    const token = uuid()
+    await db.query(
+      `INSERT INTO sessions (token, "userId") 
+    VALUES ($1, $2)`,
+      [token, id]
+    )
+    res.status(200).send({ token: token })
   } catch (err) {
     res.sendStatus(500)
   }
